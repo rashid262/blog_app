4e17d54dee12492713e76f6edac74d7436c567f3
rashid26 <rashidansari.an697@gmail.com>
2025-01-30 13:14:43 +0530

    file added

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
 from doctest import debug
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author' : 'John Doe',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'April 21, 2024'
+    },
+    {
+        'author' : 'Rashid Ansari',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'May 21, 2024'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts = posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
 
 if __name__ == "__main__":
     app.run(debug=True)
